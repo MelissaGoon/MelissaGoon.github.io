5b97f5a310e25a178ae437dfe42c95cc5060e149
MelissaGoon <melissagoon13743@gmail.com>
2024-12-30 20:37:55 +0800

    Made Loader prettier

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,11 +4,10 @@ import { Html } from "@react-three/drei";
 const Loader = () => {
     return (
         // HTML Allows you to tie HTML content to any object of your scene. It will be projected to the objects whereabouts automatically.
-        // TODO: Make loader look better
         <Html>
             <div className='flex justify-center items-center'>
-                <div className='w-20 h-20 border-2 border-opacity-20
-                border-blue-500 border-t-blue-500 rounded-full animate-spin'></div>
+                <div className='w-20 h-20 border-8 border-opacity-20
+                border-pink-500 border-t-purple-700 rounded-full animate-spin'></div>
             </div>
         </Html>
     );
